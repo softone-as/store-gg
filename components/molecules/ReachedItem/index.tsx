2fb50f7f300f9137f263585cb2c7e38294e80344
softone-as <shofwanas12@gmail.com>
2022-03-20 19:10:04 +0700

    Refactor Sections in Homepage and Detailpage

---
+++ components/molecules/ReachedItem/index.tsx
@@ -0,0 +1,18 @@
+interface ReachedItemProps {
+  value: string;
+  title: string;
+}
+
+export default function ReachedItem(props: ReachedItemProps) {
+  const { title, value } = props;
+  return (
+    <div className='me-lg-35'>
+      <p className='text-4xl text-lg-start text-center color-palette-1 fw-bold m-0'>
+        {value}
+      </p>
+      <p className='text-lg text-lg-start text-center color-palette-2 m-0'>
+        {title}
+      </p>
+    </div>
+  );
+}
